b31d32e87dbc70ff82b6b54674d99143d414a7df
Chris Lacey <clacey@fusionalliance.com>
2024-04-18 14:39:06 -0400

    get demo ready

---
+++ src/app/types.ts
@@ -0,0 +1,36 @@
+export interface StrapiResponse {
+  data: Datum;
+  meta: Meta;
+}
+
+export interface Datum {
+  id: number;
+  attributes: Attributes;
+}
+
+export interface Attributes {
+  sectionTitle: string;
+  sectionDescription: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  Card: Card[];
+}
+
+export interface Card {
+  id: number;
+  title: string;
+  description?: string;
+  date: string;
+}
+
+export interface Meta {
+  pagination: Pagination;
+}
+
+export interface Pagination {
+  page: number;
+  pageSize: number;
+  pageCount: number;
+  total: number;
+}
